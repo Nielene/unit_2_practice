268315d433c5b83e234c261262292ba0aa069acf
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-27 21:34:18 -0500

    arr of api obj made

---
+++ practice/Pscript.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  api = [
+    {route: "post", url: "https://jsonplaceholder.typicode.com/posts"},
+    {route: "comments", url: "https://jsonplaceholder.typicode.com/comments"},
+    {route: "albums", url: "https://jsonplaceholder.typicode.com/albums"},
+    {route: "photos", url: "https://jsonplaceholder.typicode.com/photos"},
+    {route: "users", url: "https://jsonplaceholder.typicode.com/users"},
+    {route: "todos", url: "https://jsonplaceholder.typicode.com/todos"},
+  ]
+
+
+
+  let posts;
+  let select = document.querySelector("select");
+
+  updateInfoPosts()
+
+  // select.addEventListener("change", updateInfo)
+
+  function updateInfoPosts() {
+    axios
+    .get("https://jsonplaceholder.typicode.com/posts")
+    .then(res => {
+    posts = res;
+    console.log(posts);
+    // debugger
+    })
+    .catch(err => {
+      console.log("error: ", err);
+    })
+  }
+
+  // function
+
+
+
+
+
+
+
+
+
+
+
+})
+
+// api[0].route // "post"
